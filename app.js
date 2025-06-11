7fd09fe147a5c94157dfa2c378a8af7da2fecd81
AdilsonMarques900 <adilson01061984@hotmail.com>
2025-06-11 19:49:46 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('Sistema Marques Last Mile carregado');
+
+function filtrar(status) {
+  alert("Filtro aplicado: " + status);
+}
+console.log("Painel Marques Last Mile com filtros carregado");
